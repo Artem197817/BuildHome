e669d05612243c3721715ce7031a47fac415b7ce
Artem <katod1978@yandex.ru>
2024-10-01 00:39:42 +0700

    v4.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function () {
     new WOW({
         animateClass: 'animate__animated',
@@ -30,25 +28,27 @@ $(document).ready(function () {
             event.preventDefault();
             event.stopPropagation();
             this.classList.add('was-validated');
-    
+
             // Валидация инпутов внутри текущей формы
             $(this).find('input').each(function () {
                 if (!this.checkValidity()) {
                     $(this).addClass('is-invalid');
-                    $(this).next().css({ display: 'block' });
+                    $(this).next().css({display: 'block'});
+                    $(this).css({borderColor: 'red'});
                 } else {
                     $(this).removeClass('is-invalid');
-                    $(this).next().css({ display: 'none' });
+                    $(this).next().css({display: 'none'});
+                    $(this).css({borderColor: '#fff'});
                 }
             });
-    
+
             // Проверка валидности всей формы
             if (this.checkValidity()) {
                 this.classList.remove('was-validated');
-    
+
                 let name = $('#inputName', this).val();
                 let phone = $('#phone', this).val();
-    
+
                 // Отправка данных формы
                 $.ajax({
                     url: 'https://testologia.ru/checkout',
@@ -59,15 +59,15 @@ $(document).ready(function () {
                     },
                     success: function (response) {
                         if (response.success === 1) {
-                           successInfo('Спасибо, мы свяжемся с вами в ближайшее время!', form);
+                            successInfo('Спасибо, мы свяжемся с вами в ближайшее время!', form);
                         } else {
-                           successInfo('Возникла ошибка при оформлении, позвоните нам и запишитесь', form);
+                            successInfo('Возникла ошибка при оформлении, позвоните нам и запишитесь', form);
                         }
                     },
                     error: function (xhr, status, error) {
                         console.error('Произошла ошибка:', error);
                     }
-                
+
                 });
             }
         });
@@ -79,91 +79,91 @@ $(document).ready(function () {
     let popUpMsg;
 
     function successInfo(message, form) {
-        if(form.id === 'consultation-form'){
-             popUpForm =$('#consultation-form');
-             popUpMessage=$('.consultation-order-message');
-             popUpMsg=$('#consultation-message')
-        }else{
-            popUpForm =$('#pop-up-form');
-             popUpMessage=$('.pop-up-message');
-              popUpMsg = $('#pop-up-msg')
+        if (form.id === 'consultation-form') {
+            popUpForm = $('#consultation-form');
+            popUpMessage = $('.consultation-order-message');
+            popUpMsg = $('#consultation-message')
+        } else {
+            popUpForm = $('#pop-up-form');
+            popUpMessage = $('.pop-up-message');
+            popUpMsg = $('#pop-up-msg')
         }
-        popUpForm.css({ display: 'none' });
-        popUpMessage.css({ display: 'flex' });
+        popUpForm.css({display: 'none'});
+        popUpMessage.css({display: 'flex'});
         popUpMsg.text(message);
         form.reset();
-       
-        
-    }
-   
 
-    $('#button-tour').click((e)=>{
+
+    }
+
+
+    $('#button-tour').click(() => {
         popUp.css('display', 'flex');
         console.log(popUp)
-        console.log( $('#button-tour'))
+        console.log($('#button-tour'))
         $('#pop-up-msg').css('display', 'flex');
     });
 
     $('.close-pop').click(() => {
-        popUp.css({ display: 'none' });
-        popUpForm.css({ display: 'flex' });
-        popUpMessage.css({ display: 'none' });
-        
-        
+        popUp.css({display: 'none'});
+        popUpForm.css({display: 'flex'});
+        popUpMessage.css({display: 'none'});
+
+
     });
 
     $(window).click((e) => {
         if ($(e.target).is(popUp)) {
-            popUp.css({ display: 'none' });
-            popUpForm.css({ display: 'flex' });
-            popUpMessage.css({ display: 'none' });
+            popUp.css({display: 'none'});
+            popUpForm.css({display: 'flex'});
+            popUpMessage.css({display: 'none'});
         }
-    }); 
+    });
 
     $('.close-order').click(() => {
-        $('#consultation-form').css({ display: 'flex' });
-        $('.consultation-order-message').css({ display: 'none' });
-        
-        
+        $('#consultation-form').css({display: 'flex'});
+        $('.consultation-order-message').css({display: 'none'});
+
+
     });
 
     const technologyInfoArrow = $('.technology-info-arrow')
 
-    $('.technology-dot').on('click', function() {
-        const classes = $(this).attr('class'); 
+    $('.technology-dot').on('click', function () {
+        const classes = $(this).attr('class');
 
         let title;
         let text;
 
-        switch (true) { 
+        switch (true) {
             case classes.includes('dot1'):
-                title ='Неразрывний каркас'
+                title = 'Неразрывний каркас'
                 text = 'Монтаж стен этажей внутри дома и по всему периметру выполняется единым массивом.'
                 break;
 
             case classes.includes('dot2'):
-                title ='5 камерные окна'
-                 text = 'Обеспечивает исключительную сохранность тепла в доме'
+                title = '5 камерные окна'
+                text = 'Обеспечивает исключительную сохранность тепла в доме'
                 break;
 
             case classes.includes('dot3'):
-                title ='Плитная ветрозащита'
+                title = 'Плитная ветрозащита'
                 text = 'Используемая влагостойкая ветрозащитная плита обеспечивает дополнительную шумоизоляцию стен'
                 break;
 
-             case classes.includes('dot4'):
-                title ='Сборка силовых узлов'
+            case classes.includes('dot4'):
+                title = 'Сборка силовых узлов'
                 text = 'Основные силовые узлы наших домов оцинкованы, что позволяет быть уверенными в исключительной прочности и долговечности конструкции'
                 break
 
-             case classes.includes('dot5'):
-                title ='Диагональный раскос'
+            case classes.includes('dot5'):
+                title = 'Диагональный раскос'
                 text = 'Система диагональных раскосов позволяет создать оптимальный вентиляционный зазор для капитальных стен и наружной отделки'
                 break;
-    
+
             default:
                 console.log('No matching class found');
-              
+
         }
         technologyInfoArrow.css();
         $('.technology-info-title.min').text(title);
@@ -173,25 +173,25 @@ $(document).ready(function () {
 
     $(window).click((e) => {
         if ($(e.target).is(technologyInfoArrow)) {
-            technologyInfoArrow.css({ display: 'none' });
+            technologyInfoArrow.css({display: 'none'});
         }
-    }); 
+    });
 
     let menu = $('.menu-pop-up');
 
-    $('.menu').click(()=>{
+    $('.menu').click(() => {
         menu.css('display', 'block');
     });
 
-    $('.close-menu').click(()=>{
+    $('.close-menu').click(() => {
         menu.css('display', 'none');
     })
 
     $(window).click((e) => {
         if ($(e.target).is(menu)) {
-            menu.css({ display: 'none' });
+            menu.css({display: 'none'});
         }
-    }); 
+    });
 
     function updateLeftPosition() {
         const fixedElement = document.querySelector('.my-arrow-down');
@@ -200,23 +200,23 @@ $(document).ready(function () {
         let leftPosition;
 
         if (containerWidth >= 1920) {
-            leftPosition = 305; 
+            leftPosition = 305;
         } else if (containerWidth <= 1200) {
-            leftPosition = -55; 
+            leftPosition = -55;
         } else {
             leftPosition = (containerWidth - 1200) / 2 - 70;
         }
 
         fixedElement.style.left = `${leftPosition}px`;
     }
-    
+
     window.addEventListener('load', updateLeftPosition);
     window.addEventListener('resize', updateLeftPosition);
 
     window.addEventListener('scroll', () => {
 
         if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-            consultationPopUp.css({ display: 'flex' });
+            consultationPopUp.css({display: 'flex'});
         }
     });
 
@@ -224,13 +224,13 @@ $(document).ready(function () {
 
     $('.consultation-pop-up-link').click(() => {
 
-        consultationPopUp.css({ display: 'none' });
+        consultationPopUp.css({display: 'none'});
 
     });
 
     $(window).click((e) => {
         if ($(e.target).is(consultationPopUp)) {
-           consultationPopUp.css({ display: 'none' });
+            consultationPopUp.css({display: 'none'});
         }
     });
 
@@ -245,8 +245,8 @@ $(document).ready(function () {
 
         }
     });
-    $('.project-link-block').click(()=>{
-        $('.more').css({ display: 'flex' });
+    $('.project-link-block').click(() => {
+        $('.more').css({display: 'flex'});
     });
 
 });
